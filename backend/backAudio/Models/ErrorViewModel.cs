a0e20e71059112289efd781161b2d49b20c8dcae
Ad4m-eve <len3500tab@gmail.com>
2023-07-07 23:25:30 +0300

    add asp.net core web app and change sql_script

---
+++ backend/backAudio/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace backAudio.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
--- backend/sql_script.sql
+++ backend/sql_script.sql
@@ -2,3 +2,27 @@ create schema musicpulse
 default character set cp1251
 collate cp1251_ukrainian_ci;
 
+use musicpulse;
+
+CREATE TABLE user_
+(
+	user_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+	user_name VARCHAR(255) NOT NULL,
+    user_password VARCHAR(20) UNIQUE NOT NULL
+);
+
+CREATE TABLE song_
+(
+	song_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+	song_name VARCHAR(255) NOT NULL,
+    song_author VARCHAR(255) NOT NULL,
+    song_singer VARCHAR(255) NOT NULL,
+    song_description VARCHAR(1000)
+);
+
+CREATE TABLE favorites
+( 
+	favorites_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    song_id INT(11) NOT NULL,
+    user_id INT(11) NOT NULL
+);
